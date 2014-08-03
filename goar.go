3e686ba3263b628b351c1f78dc0c68394999a436
Bin Zhang <binz@twitter.com>
2014-08-03 00:24:12 -0700

    Goar library and command line tool.

---
+++ goar.go
@@ -0,0 +1,248 @@
+package goar
+
+import (
+	"archive/zip"
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+const suffix = "GOAR"
+
+type Archiver struct {
+	f        *os.File
+	fw       *bufio.Writer
+	fileSize int64
+	zw       *zip.Writer
+	err      error
+}
+
+func NewArchiver(fn string) (*Archiver, error) {
+	f, err := os.OpenFile(fn, os.O_WRONLY|os.O_APPEND, 0755)
+	if err != nil {
+		return nil, err
+	}
+	fw := bufio.NewWriter(f)
+	fi, err := f.Stat()
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+	return &Archiver{
+		f:        f,
+		fw:       fw,
+		fileSize: fi.Size(),
+		zw:       zip.NewWriter(fw),
+	}, nil
+}
+
+func (ar *Archiver) Add(fn string) error {
+	if ar.err != nil {
+		return ar.err
+	}
+
+	in, err := os.Open(fn)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	fn = filepath.Clean(fn)
+	fn = filepath.ToSlash(fn)
+	fn = strings.TrimLeft(fn, "/") // make it relative to cwd
+
+	out, err := ar.zw.Create(fn)
+	if ar.setErr(err) != nil {
+		return ar.err
+	}
+	_, err = io.Copy(out, in)
+	return ar.setErr(err)
+}
+
+func (ar *Archiver) setErr(err error) error {
+	if ar.err == nil {
+		ar.err = err
+	}
+	return ar.err
+}
+
+func (ar *Archiver) Close() error {
+	if ar.f == nil {
+		return ar.err
+	}
+	ar.setErr(ar.zw.Close())
+	if ar.err == nil {
+		b := make([]byte, 8+len(suffix))
+		binary.BigEndian.PutUint64(b, uint64(ar.fileSize))
+		copy(b[8:], suffix)
+		_, err := ar.fw.Write(b)
+		ar.setErr(err)
+	}
+	if ar.err == nil {
+		ar.setErr(ar.fw.Flush())
+	}
+	fn := ar.f.Name()
+	ar.setErr(ar.f.Close())
+
+	if ar.err != nil {
+		// If anything goes wrong, restore the file
+		if err := os.Truncate(fn, ar.fileSize); err != nil {
+			// The file is corrupted probably. No way to report the error other than panic.
+			panic(err)
+		}
+	}
+
+	err := ar.err
+	*ar = Archiver{err: io.ErrClosedPipe} // zero out to mean it is closed
+	return err
+}
+
+type FileInfo struct {
+	Name string
+	Size int64
+}
+
+type File struct {
+	FileInfo
+	io.ReadCloser
+}
+
+type FileSystem struct {
+	BinarySize int64
+	f          *os.File
+	zr         *zip.Reader
+	files      map[string]*zip.File
+	mu         sync.RWMutex
+}
+
+func NewFileSystem(fn string) (*FileSystem, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	start, end, err := readZipRegion(f)
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+	tr := &tailReader{
+		r:   f,
+		off: start,
+	}
+	zr, err := zip.NewReader(tr, end-start)
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+	files := make(map[string]*zip.File)
+	for _, zf := range zr.File {
+		files[zf.Name] = zf
+	}
+	return &FileSystem{
+		BinarySize: start,
+		f:          f,
+		zr:         zr,
+		files:      files,
+	}, nil
+}
+
+func readZipRegion(f *os.File) (start, end int64, err error) {
+	n := int64(8 + len(suffix))
+	if end, err = f.Seek(-n, 2); err != nil {
+		return 0, 0, err
+	}
+	b := make([]byte, 8+len(suffix))
+	if _, err = io.ReadFull(f, b); err != nil {
+		return 0, 0, err
+	}
+	if !bytes.HasSuffix(b, []byte(suffix)) {
+		return 0, 0, fmt.Errorf("file %s not end with %s", f.Name(), suffix)
+	}
+	start = int64(binary.BigEndian.Uint64(b[:8]))
+	return start, end, nil
+}
+
+type tailReader struct {
+	r   io.ReaderAt
+	off int64
+}
+
+func (tr *tailReader) ReadAt(p []byte, off int64) (int, error) {
+	return tr.r.ReadAt(p, tr.off+off)
+}
+
+func (fs *FileSystem) Open(name string) (*File, error) {
+	fs.mu.RLock()
+	defer fs.mu.RUnlock()
+
+	zf, ok := fs.files[name]
+	if !ok {
+		return nil, fmt.Errorf("file %s not found", name)
+	}
+	rc, err := zf.Open()
+	if err != nil {
+		return nil, err
+	}
+	return &File{
+		FileInfo: FileInfo{
+			Name: zf.Name,
+			Size: int64(zf.UncompressedSize64),
+		},
+		ReadCloser: rc,
+	}, nil
+}
+
+func (fs *FileSystem) List() []*FileInfo {
+	fs.mu.RLock()
+	defer fs.mu.RUnlock()
+
+	fns := make([]*FileInfo, 0, len(fs.files))
+	for _, zf := range fs.files {
+		fns = append(fns, &FileInfo{
+			Name: zf.Name,
+			Size: int64(zf.UncompressedSize64),
+		})
+	}
+	return fns
+}
+
+func (fs *FileSystem) Close() error {
+	fs.mu.Lock()
+	defer fs.mu.Unlock()
+
+	err := fs.f.Close()
+	fs.f = nil
+	fs.zr = nil
+	fs.files = nil
+	return err
+}
+
+var (
+	progFS *FileSystem
+	once   sync.Once
+)
+
+func initProgFS() {
+	fn := os.Args[0]
+	fs, err := NewFileSystem(fn)
+	if err != nil {
+		panic(fmt.Sprintf("failed to init goar file system from %s, err:%v", fn, err))
+	}
+	progFS = fs
+}
+
+func Open(name string) (*File, error) {
+	once.Do(initProgFS)
+	return progFS.Open(name)
+}
+
+func List() []*FileInfo {
+	once.Do(initProgFS)
+	return progFS.List()
+}
---
+++ main/goar.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/zncode/goar"
+)
+
+func main() {
+	archiveOpt := flag.Bool("a", false, "pack files to go binary")
+	inspectOpt := flag.Bool("t", false, "inspect goar file")
+	trimOpt := flag.Bool("r", false, "trim goar file to restore the original binary")
+	extractOpt := flag.Bool("e", false, "extract files")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage:
+  goar -t <go_binary>
+  goar -a <go_binary> <file>...
+  goar -r <go_binary>
+  goar -e <go_binary> <file>...
+
+`)
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	flag.Parse()
+
+	switch {
+	case *archiveOpt:
+		if flag.NArg() < 2 {
+			flag.Usage()
+		}
+		archive(flag.Arg(0), flag.Args()[1:])
+
+	case *inspectOpt:
+		if flag.NArg() != 1 {
+			flag.Usage()
+		}
+		inspect(flag.Arg(0))
+
+	case *trimOpt:
+		if flag.NArg() != 1 {
+			flag.Usage()
+		}
+		trim(flag.Arg(0))
+
+	case *extractOpt:
+		if flag.NArg() < 1 {
+			flag.Usage()
+		}
+		extract(flag.Arg(0), flag.Args()[1:])
+
+	default:
+		flag.Usage()
+	}
+}
+
+func inspect(fn string) {
+	fs, err := goar.NewFileSystem(fn)
+	if err != nil {
+		log.Fatalf("inspect file:%s err:%v", fn, err)
+	}
+	defer fs.Close()
+
+	fmt.Printf("Size of binary: %d\n", fs.BinarySize)
+	for _, fi := range fs.List() {
+		fmt.Printf("  %s => %d\n", fi.Name, fi.Size)
+	}
+}
+
+func archive(binfn string, fns []string) {
+	ar, err := goar.NewArchiver(binfn)
+	if err != nil {
+		log.Fatalf("new archiver err:%v", err)
+	}
+	defer ar.Close()
+
+	for _, fn := range fns {
+		if err = ar.Add(fn); err != nil {
+			log.Fatalf("add file:%s err:%v", fn, err)
+		}
+		log.Printf("file:%s added", fn)
+	}
+}
+
+func trim(fn string) {
+	fs, err := goar.NewFileSystem(fn)
+	if err != nil {
+		log.Fatalf("open goar file:%s err:%v", fn, err)
+	}
+	sz := fs.BinarySize
+	fs.Close()
+
+	if err = os.Truncate(fn, sz); err != nil {
+		log.Fatalf("truncate file:%s to size:%d err:%v", fn, sz, err)
+	}
+	log.Printf("trim to %d", sz)
+}
+
+func extract(binfn string, fns []string) {
+	fs, err := goar.NewFileSystem(binfn)
+	if err != nil {
+		log.Fatalf("inspect file:%s err:%v", binfn, err)
+	}
+	defer fs.Close()
+
+	if len(fns) == 0 {
+		for _, fi := range fs.List() {
+			fns = append(fns, fi.Name)
+		}
+	}
+
+	for _, fn := range fns {
+		f, err := fs.Open(fn)
+		if err != nil {
+			log.Printf("fail to extract file:%s err:%v", fn, err)
+			continue
+		}
+		dn := filepath.Dir(fn)
+		if err := os.MkdirAll(dn, 0755); err != nil {
+			log.Fatalf("cannot make dir:%s err:%v", dn, err)
+		}
+		out, err := os.Create(fn)
+		if err != nil {
+			log.Fatalf("cannot create file:%s err:%v", fn, err)
+		}
+		_, err = io.Copy(out, f)
+		if err != nil {
+			log.Fatalf("fail to extract file:%s err:%v", fn, err)
+		}
+		f.Close()
+		out.Close()
+	}
+}
